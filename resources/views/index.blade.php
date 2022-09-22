a901fd74cc692297a4cda6f16f08f0e0329f07d4
Deden <dedenh018@gmail.com>
2022-09-22 08:17:37 +0700

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Rumah</title>
+
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+
+<body background="/img/3.jpg">
+    <div class="container mt-5">
+        <div>
+            <img src="/img/1.jpg" class="rounded float-left" height="200">
+        </div>
+        <div>
+            <br><br>
+            <h1 class="font-weight-bold, display-4">&nbsp;&nbsp;&nbsp;<?= $nama ?></h1>
+            <h5 class="font-weight-bold">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;2020130054
+            </h5>
+        </div><br><br><br><br>
+
+
+        <h1 class="text-light bg-dark"><img src="/img/5.png" class="rounded float-left" height="40">PERSONAL</h1>
+        <ul>
+            <li>
+                TTL&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;Bandung 15-08-2001
+            </li>
+            <li>
+                Alamat&nbsp;:&nbsp;Rt03 Rw07 Desa Karyawangi
+            </li>
+            <li>
+                Agama&nbsp;&nbsp;:&nbsp;Islam
+            </li>
+        </ul>
+
+        <h1 class="text-light bg-dark"><img src="/img/7.png" class="rounded float-left" height="40">&nbsp;RIWAYAT PENDIDIKAN</h1>
+        <ul>
+            <li>
+                TK RA ASSHAFA
+            </li>
+            <li>
+                SDN Harpan Mulya
+            </li>
+            <li>
+                SMPN 1 Parongpong
+            </li>
+            <li>
+                SMAN 1 Parongpong
+            </li>
+        </ul>
+
+        <h1 class="text-light bg-dark"><img src="/img/11.png" class="rounded float-left" height="40">KONTAK</h1>
+        <ul>
+            <li>
+                <img src="/img/8.png"  height="40">@deden0815
+
+            </li>
+            <li>
+                <img src="/img/9.png" height="40">0895356487418
+
+            </li>
+            <li>
+                <img src="/img/10.png"  height="40">dedenh019@gmail.com
+
+            </li>
+        </ul>
+
+    </div>
+
+    <script src="/js/app.js"></script>
+</body>
+
+</html>
---
+++ resources/views/portfolio.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Rumah</title>
+
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+
+<body background="/img/3.jpg">
+    <div class="container mt-5">
+        <div>
+            <img src="/img/1.jpg" class="rounded float-left" height="200">
+        </div>
+        <div>
+            <br><br>
+            <h1 class="font-weight-bold, display-4">&nbsp;&nbsp;&nbsp;<?= $nama ?></h1>
+            <h5 class="font-weight-bold">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;2020130054
+            </h5>
+        </div><br><br><br><br>
+
+
+        <h1 class="text-light bg-dark">KARYA</h1>
+
+        <div class="card" style="width: 18rem;">
+            <img src="img/12.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">CorelDRAW</h5>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img src="img/13.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">3ds Max</h5>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img src="img/14.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">3ds Max</h5>
+            </div>
+        </div>
+
+
+
+    </div>
+
+    <script src="/js/app.js"></script>
+</body>
+
+</html>
